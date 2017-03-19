aa1694d87990189e23675c33d24687ecec4b9ead
PierreBtz <PierreBtz@users.noreply.github.com>
2017-03-19 15:37:03 +0100

    Initial commit, spring project generated from spring initilalizr

---
+++ src/main/java/eu/beitz/JenkinsMeetupDemoApplication.java
@@ -0,0 +1,12 @@
+package eu.beitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMeetupDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsMeetupDemoApplication.class, args);
+	}
+}
