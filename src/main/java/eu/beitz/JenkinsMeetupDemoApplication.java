f104c1274759a3455090956edc5d3fdefb6cca92
PierreBtz <PierreBtz@users.noreply.github.com>
2017-03-19 15:38:23 +0100

    Apply Intellij formatting

--- src/main/java/eu/beitz/JenkinsMeetupDemoApplication.java
+++ src/main/java/eu/beitz/JenkinsMeetupDemoApplication.java
@@ -3,10 +3,13 @@ package eu.beitz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by Pierre Btz on 19/03/17.
+ */
 @SpringBootApplication
 public class JenkinsMeetupDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinsMeetupDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsMeetupDemoApplication.class, args);
+    }
 }
